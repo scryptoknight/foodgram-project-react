88b6d3c8cbb89725e83f2ce57ba8014d479fed3e
Aleksandr Repin <sonoffjord@gmail.com>
2021-09-30 21:22:31 +0300

    deploy15

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'DatabaseName',
+        'USER': 'DatabaseUserName',
+        'PASSWORD': 'DatabaseUserpassword',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
